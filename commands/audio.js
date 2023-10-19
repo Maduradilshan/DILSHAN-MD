6f2fd5048c02f0cf27aa4ee2cbd69b315474e361
Maduradilshan <140952508+Maduradilshan@users.noreply.github.com>
2023-10-19 15:52:05 +0530

    Update audio.js

--- commands/audio.js
+++ commands/audio.js
@@ -4,13 +4,8 @@
 //                                ＷＨＡＴＳＡＰＰ ＢＯＴ－ＭＤ ＢＥＴＡ                                   //
 //                                                                                                      // 
 //                                         Ｖ：１．０．１                                                // 
-//                                                                                                      // 
-//            ███████╗██╗   ██╗██╗  ██╗ █████╗ ██╗██╗         ███╗   ███╗██████╗                        //
-//            ██╔════╝██║   ██║██║  ██║██╔══██╗██║██║         ████╗ ████║██╔══██╗                       //
-//            ███████╗██║   ██║███████║███████║██║██║         ██╔████╔██║██║  ██║                       //
-//            ╚════██║██║   ██║██╔══██║██╔══██║██║██║         ██║╚██╔╝██║██║  ██║                       //
-//            ███████║╚██████╔╝██║  ██║██║  ██║██║███████╗    ██║ ╚═╝ ██║██████╔╝                       //
-//            ╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚══════╝    ╚═╝     ╚═╝╚═════╝                        //
+//                                          
+//                                    
 //                                                                                                      //
 //                                                                                                      //
 //                                                                                                      //
@@ -18,16 +13,16 @@
 
 CURRENTLY RUNNING ON BETA VERSION!!
 *
-   * @project_name : Suhail-Md
-   * @author : Suhail Tech Info
-   * @youtube : https://www.youtube.com/c/@SuhailTechInfo0
-   * @description : Suhail-Md ,A Multi-functional whatsapp user bot.
+   * @project_name : DILSHAN-Md
+   * @author : DILSHAN Tech Info
+   * @youtube : https://www.youtube.com/c/@DILSHANTechInfo0
+   * @description : DILSHAN-Md ,A Multi-functional whatsapp user bot.
    * @version 1.2.2
 *
    * Licensed under the  GPL-3.0 License;
 * 
-   * Created By Suhail Tech Info.
-   * © 2023 Suhail-Md.
+   * Created By DILSHAN Tech Info.
+   * © 2023 DILSHAN-Md.
 * 
    * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
    * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -45,27 +40,27 @@ const { exec } = require('child_process')
 
 
 //---------------------------------------------------------------------------
-smd({pattern: "bass",desc: "adds bass in given sound",category: "audio",use: '<reply to any audio>',},async(Suhail, msg , text , {cmdName , isCreator}) => {try{return await audioEditor(Suhail,msg,cmdName,msg);}catch(e){ return await msg.error(e),console.log("Error in Audio Editing\n",e); } })
+smd({pattern: "bass",desc: "adds bass in given sound",category: "audio",use: '<reply to any audio>',},async(DILSHAN, msg , text , {cmdName , isCreator}) => {try{return await audioEditor(DILSHAN,msg,cmdName,msg);}catch(e){ return await msg.error(e),console.log("Error in Audio Editing\n",e); } })
 //---------------------------------------------------------------------------
-smd({pattern: "blown",desc: "adds blown in given sound",category: "audio",use: '<reply to any audio>',},async(Suhail, msg , text , {cmdName , isCreator}) => {try{return await audioEditor(Suhail,msg,cmdName,msg);}catch(e){ return await msg.error(e),console.log("Error in Audio Editing\n",e); } })
+smd({pattern: "blown",desc: "adds blown in given sound",category: "audio",use: '<reply to any audio>',},async(DILSHAN, msg , text , {cmdName , isCreator}) => {try{return await audioEditor(DILSHAN,msg,cmdName,msg);}catch(e){ return await msg.error(e),console.log("Error in Audio Editing\n",e); } })
 //---------------------------------------------------------------------------
-smd({pattern: "deep",desc: "adds deep in given sound",category: "audio",use: '<reply to any audio>',},async(Suhail, msg , text , {cmdName , isCreator}) => {try{return await audioEditor(Suhail,msg,cmdName,msg);}catch(e){ return await msg.error(e),console.log("Error in Audio Editing\n",e); } })
+smd({pattern: "deep",desc: "adds deep in given sound",category: "audio",use: '<reply to any audio>',},async(DILSHAN, msg , text , {cmdName , isCreator}) => {try{return await audioEditor(DILSHAN,msg,cmdName,msg);}catch(e){ return await msg.error(e),console.log("Error in Audio Editing\n",e); } })
 //---------------------------------------------------------------------------
-smd({pattern: "earrape",desc: "adds earrape in given sound",category: "audio",use: '<reply to any audio>',},async(Suhail, msg , text , {cmdName , isCreator}) => {try{return await audioEditor(Suhail,msg,cmdName,msg);}catch(e){ return await msg.error(e),console.log("Error in Audio Editing\n",e); } })
+smd({pattern: "earrape",desc: "adds earrape in given sound",category: "audio",use: '<reply to any audio>',},async(DILSHAN, msg , text , {cmdName , isCreator}) => {try{return await audioEditor(DILSHAN,msg,cmdName,msg);}catch(e){ return await msg.error(e),console.log("Error in Audio Editing\n",e); } })
 //---------------------------------------------------------------------------
-smd({pattern: "fast",desc: "adds fast in given sound",category: "audio",use: '<reply to any audio>',},async(Suhail, msg , text , {cmdName , isCreator}) => {try{return await audioEditor(Suhail,msg,cmdName,msg);}catch(e){ return await msg.error(e),console.log("Error in Audio Editing\n",e); } })
+smd({pattern: "fast",desc: "adds fast in given sound",category: "audio",use: '<reply to any audio>',},async(DILSHAN, msg , text , {cmdName , isCreator}) => {try{return await audioEditor(DILSHAN,msg,cmdName,msg);}catch(e){ return await msg.error(e),console.log("Error in Audio Editing\n",e); } })
 //---------------------------------------------------------------------------
-smd({pattern: "fat",desc: "adds fat in given sound",category: "audio",use: '<reply to any audio>',},async(Suhail, msg , text , {cmdName , isCreator}) => {try{return await audioEditor(Suhail,msg,cmdName,msg);}catch(e){ return await msg.error(e),console.log("Error in Audio Editing\n",e); } })
+smd({pattern: "fat",desc: "adds fat in given sound",category: "audio",use: '<reply to any audio>',},async(DILSHAN , msg , text , {cmdName , isCreator}) => {try{return await audioEditor(DILSHAN,msg,cmdName,msg);}catch(e){ return await msg.error(e),console.log("Error in Audio Editing\n",e); } })
 //---------------------------------------------------------------------------
-smd({pattern: "nightcore",desc: "adds nightcore in given sound",category: "audio",use: '<reply to any audio>',},async(Suhail, msg , text , {cmdName , isCreator}) => {try{return await audioEditor(Suhail,msg,cmdName,msg);}catch(e){ return await msg.error(e),console.log("Error in Audio Editing\n",e); } })
+smd({pattern: "nightcore",desc: "adds nightcore in given sound",category: "audio",use: '<reply to any audio>',},async(DILSHAN, msg , text , {cmdName , isCreator}) => {try{return await audioEditor(DILSHAN,msg,cmdName,msg);}catch(e){ return await msg.error(e),console.log("Error in Audio Editing\n",e); } })
 //---------------------------------------------------------------------------
-smd({pattern: "reverse",desc: "adds reverse in given sound",category: "audio",use: '<reply to any audio>',},async(Suhail, msg , text , {cmdName , isCreator}) => {try{return await audioEditor(Suhail,msg,cmdName,msg);}catch(e){ return await msg.error(e),console.log("Error in Audio Editing\n",e); } })
+smd({pattern: "reverse",desc: "adds reverse in given sound",category: "audio",use: '<reply to any audio>',},async(DILSHAN, msg , text , {cmdName , isCreator}) => {try{return await audioEditor(DILSHAN,msg,cmdName,msg);}catch(e){ return await msg.error(e),console.log("Error in Audio Editing\n",e); } })
 //---------------------------------------------------------------------------
-smd({pattern: "robot",desc: "adds robot in given sound",category: "audio",use: '<reply to any audio>',},async(Suhail, msg , text , {cmdName , isCreator}) => {try{return await audioEditor(Suhail,msg,cmdName,msg);}catch(e){ return await msg.error(e),console.log("Error in Audio Editing\n",e); } })
+smd({pattern: "robot",desc: "adds robot in given sound",category: "audio",use: '<reply to any audio>',},async(DILSHAN, msg , text , {cmdName , isCreator}) => {try{return await audioEditor(DILSHAN,msg,cmdName,msg);}catch(e){ return await msg.error(e),console.log("Error in Audio Editing\n",e); } })
 //---------------------------------------------------------------------------
-smd({pattern: "slow",desc: "adds slow in given sound",category: "audio",use: '<reply to any audio>',},async(Suhail, msg , text , {cmdName , isCreator}) => {try{return await audioEditor(Suhail,msg,cmdName,msg);}catch(e){ return await msg.error(e),console.log("Error in Audio Editing\n",e); } })
+smd({pattern: "slow",desc: "adds slow in given sound",category: "audio",use: '<reply to any audio>',},async(DILSHAN, msg , text , {cmdName , isCreator}) => {try{return await audioEditor(DILSHAN,msg,cmdName,msg);}catch(e){ return await msg.error(e),console.log("Error in Audio Editing\n",e); } })
 //---------------------------------------------------------------------------
-smd({pattern: "smooth",desc: "adds smooth in given sound",category: "audio",use: '<reply to any audio>',},async(Suhail, msg , text , {cmdName , isCreator}) => {try{return await audioEditor(Suhail,msg,cmdName,msg);}catch(e){ return await msg.error(e),console.log("Error in Audio Editing\n",e); } })
+smd({pattern: "smooth",desc: "adds smooth in given sound",category: "audio",use: '<reply to any audio>',},async(DILSHAN, msg , text , {cmdName , isCreator}) => {try{return await audioEditor(DILSHAN,msg,cmdName,msg);}catch(e){ return await msg.error(e),console.log("Error in Audio Editing\n",e); } })
 //---------------------------------------------------------------------------
-smd({pattern: "tupai",desc: "adds tupai in given sound",category: "audio",use: '<reply to any audio>',},async(Suhail, msg , text , {cmdName , isCreator}) => {try{return await audioEditor(Suhail,msg,cmdName,msg);}catch(e){ return await msg.error(e),console.log("Error in Audio Editing\n",e); } })
+smd({pattern: "tupai",desc: "adds tupai in given sound",category: "audio",use: '<reply to any audio>',},async(DILSHAN, msg , text , {cmdName , isCreator}) => {try{return await audioEditor(DILSHAN,msg,cmdName,msg);}catch(e){ return await msg.error(e),console.log("Error in Audio Editing\n",e); } })
 //---------------------------------------------------------------------------
